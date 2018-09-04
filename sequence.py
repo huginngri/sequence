f3dd35300d1ff1c81109660c095430daad41a745
huginngri <huginngri@gmail.com>
2018-09-04 10:33:08 +0000

    Algorithm written for sequence

--- sequence.py
+++ sequence.py
@@ -7,18 +7,23 @@ num_3 = 3
 num_4 = 0
 counter = 3
 if n == 1:
-    print(num_2)
+    print(num_1)
 elif n == 2:
-    print(num_1,",", num_2)
+    print(num_1)
+    print(num_2)
 elif n == 3:
-    print(num_1,",", num_2, ",", num_3)
+    print(num_1)
+    print(num_2)
+    print(num_3)
 elif n < 1:
     print("Invalid length")
 else:
-    print(num_1,",", num_2,",", num_3, end=",")
+    print(num_1)
+    print(num_2)
+    print(num_3)
     while counter < n:
         num_4 = num_1 + num_2 + num_3
-        print(num_4, end=",")
+        print(num_4)
         num_1 = num_2
         num_2 = num_3
         num_3 = num_4
